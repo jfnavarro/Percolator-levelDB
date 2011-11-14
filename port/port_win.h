081e624c31aa00ccffe0795d52bfc1803b91a3d8
Jose Fernandez <jc.fernandez.navarro@gmail.com>
2011-11-14 14:07:08 +0100

    added toolchain

--- port/port_win.h
+++ port/port_win.h
@@ -35,6 +35,21 @@
 #define close _close
 #define fread_unlocked _fread_nolock
 
+#ifndef nullptr
+const                        // this is a const object...
+class {
+public:
+  template<class T>          // convertible to any type
+    operator T*() const      // of null non-member
+    { return 0; }            // pointer...
+  template<class C, class T> // or any type of null
+    operator T C::*() const  // member pointer...
+    { return 0; }
+private:
+  void operator&() const;    // whose address can't be taken
+} nullptr = {};  
+#endif
+
 #include <string>
 
 #include <stdint.h>

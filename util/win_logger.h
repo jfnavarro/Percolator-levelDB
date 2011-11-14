2a7d6de9ebf61dabd91fe3491b0cae2594112a0c
Jose Fernandez <jc.fernandez.navarro@gmail.com>
2011-11-14 16:02:35 +0100

    added toolchain

--- util/win_logger.h
+++ util/win_logger.h
@@ -17,10 +17,10 @@ class WinLogger : public Logger {
   FILE* file_;
  public:
   explicit WinLogger(FILE* f) : file_(f) { assert(file_); }
-  ~WinLogger() {
+  virtual ~WinLogger() {
     fclose(file_);
   }
-  void Logv(const char* format, va_list ap);
+  //virtual void Logv(const char* format, va_list ap);
 
 };
 
